f4923bd3e4e6b4d42d2560d9ac0e1d737edc23f5
=ryusaplad <changsaplad@gmail.com>
2022-11-01 13:54:11 +0800

    Change to single page and add some admin pages.

--- src/main/java/svfc_rdms/rdms/controller/AdminController.java
+++ src/main/java/svfc_rdms/rdms/controller/AdminController.java
@@ -1,6 +1,7 @@
 package svfc_rdms.rdms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,28 +14,40 @@ public class AdminController {
      }
 
      @GetMapping("/facilitators")
-     public String facilitatorAccounts_View() {
-          return "/view_facilitators";
+     public String facilitatorAccounts_View(Model model) {
+          model.addAttribute("title", "Facilitator Accounts");
+          return "/view_accounts";
      }
 
      @GetMapping("/registrars")
-     public String registrarAccounts_View() {
-          return "/view_registrars";
+     public String registrarAccounts_View(Model model) {
+          model.addAttribute("title", "Registrar Accounts");
+
+          return "/view_accounts";
      }
 
      @GetMapping("/teachers")
-     public String teacherAccounts_View() {
-          return "/view_teachers";
+     public String teacherAccounts_View(Model model) {
+          model.addAttribute("title", "Teachers Accounts");
+
+          return "/view_accounts";
      }
 
      @GetMapping("/students")
-     public String studentAccounts_View() {
-          return "/view_students";
+     public String studentAccounts_View(Model model) {
+          model.addAttribute("title", "Students Accounts");
+
+          return "/view_accounts";
      }
 
      @GetMapping("/all_request")
      public String viewAllRequests() {
           return "/all_requests";
+     }
+
+     @GetMapping("/admin-request")
+     public String requestForAdmin() {
+          return "/request_cards";
      }
 
      @GetMapping("/admin_logs")
